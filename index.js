2d21ca9b5fc2b63023ce1942d4d948dc3629e2f0
Arizaki <79724270+Oreki-ASTRAL@users.noreply.github.com>
2022-08-12 21:47:02 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+Const astral = require('astral.js@6.0.4')
+Const db = require('astral.ts')
+
+Const config = require('config.json')
+
+Token.new.start
+Astral.event.load('tout les évents ont été lancer avec succès !')
+
+astral.new.connect('connecter au ${client} qui possède ${serveur} serveurs !')
+
+astral.commande{(
+Nom: "ping",
+Code: `la latence de votre bot est de As${ping} ms.`
+)}
+
+astral.config.token('login.success')
